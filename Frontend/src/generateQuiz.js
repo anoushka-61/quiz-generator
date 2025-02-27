e9efbb9d16b04ea28f08fbbab13f91da9ec39612
Anoushka Srivastava <anoushka.srivastava@timesgroup.com>
2025-02-28 03:33:42 +0530

    pushing for testing resume scenario

--- Frontend/src/generateQuiz.js
+++ Frontend/src/generateQuiz.js
@@ -25,11 +25,11 @@ import { Sparkles } from "lucide-react"; // Using lucide-react for the icon
 import axios from "axios";
 import AWS from 'aws-sdk';
 
-AWS.config.update({
-  accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID, 
-  secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY, 
-  region: 'us-east-1', // Adjust the region if needed
-});
+// AWS.config.update({
+//   accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID, 
+//   secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY, 
+//   region: 'us-east-1', // Adjust the region if needed
+// });
 
 const s3 = new AWS.S3();
 const QuizGenerator = () => {
